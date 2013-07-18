0cb8ab7cdbf2e8663ca2a82b6714743b9e8e46d8
caroline chalak <caroline.chalak@3sr-grenoble.fr>
2013-07-18 15:47:33 +0200

    new physics tupe for capillary law including the interfacial energies of the wetting non-wetting phases

---
+++ pkg/dem/CapillaryPhys1.cpp
@@ -0,0 +1,16 @@
+#include <yade/pkg/dem/CapillaryPhys1.hpp>
+
+CapillaryPhys1::~CapillaryPhys1()
+{
+}
+
+YADE_PLUGIN((CapillaryPhys1));
+
+
+
+EnergeticCapillaryPhys::~EnergeticCapillaryPhys()
+{
+}
+
+YADE_PLUGIN((EnergeticCapillaryPhys));
+
