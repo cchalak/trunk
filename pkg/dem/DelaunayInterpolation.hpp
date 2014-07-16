90afa6db9d165b1c232a33d4399c48b13088c6b6
Bruno Chareyre <bruno.chareyre@hmg.inpg.fr>
2014-07-16 20:52:28 +0200

    fix transition from Pbased to Vbased interpolation

--- pkg/dem/DelaunayInterpolation.hpp
+++ pkg/dem/DelaunayInterpolation.hpp
@@ -121,9 +121,10 @@ template <class Dt, class DataOwner>
 typename DataOwner::Data interpolate1 (const Dt& dt, const typename Dt::Geom_traits::Point_3& Q, DataOwner& owner, const std::vector<typename DataOwner::Data>& rawData, bool reset)
 {
     K::FT norm;
-    Vertex_weight_vector coords;
+    Vertex_weight_vector coords;    
+    if (reset) owner.cell = dt.cells_begin();
     CGAL::Triple<std::back_insert_iterator<Vertex_weight_vector>,K::FT, bool> result = CGAL::getIncidentVtxWeights(dt, Q,std::back_inserter(coords), norm, owner.normals , reset? dt.cells_begin() : owner.cell);
-    
+     
     typename DataOwner::Data data = typename DataOwner::Data();//initialize null solution
     if (!result.third) return data;// out of the convex hull, we return the null solution
     //else, we compute the weighted sum
@@ -136,6 +137,7 @@ typename DataOwner::Data interpolate2 (const Dt& dt, const typename Dt::Geom_tra
 {
     K::FT norm;
     Vertex_weight_vector coords;
+    if (reset) owner.cell = dt.cells_begin();
     CGAL::Triple<std::back_insert_iterator<Vertex_weight_vector>,K::FT, bool> result = CGAL::getIncidentVtxWeights(dt, Q,std::back_inserter(coords), norm, owner.normals , reset? dt.cells_begin() : owner.cell);
 
     typename DataOwner::Data data = typename DataOwner::Data();//initialize null solution
